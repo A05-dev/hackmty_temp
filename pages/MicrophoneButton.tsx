3659fc56191963a643e82936cec9798cbb9b5f26
Andrés Martínez <autummata@gmail.com>
2023-09-23 17:33:12 -0600

    other changes

---
+++ pages/MicrophoneButton.tsx
@@ -0,0 +1,13 @@
+// components/MicrophoneButton.js
+import React from 'react';
+
+
+const MicrophoneButton = () => {
+  return (
+    <button className="microphone-button">
+      MICROFONO
+    </button>
+  );
+};
+
+export default MicrophoneButton;
